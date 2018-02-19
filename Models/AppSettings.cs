66a38a5c1665f05f724235312a5dd15b4fbee550
CloudsSurfer <cloudsandsurfing@github.com>
2018-02-19 20:24:18 +0300

    Add project files.

---
+++ Models/AppSettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TelegramBotApp.Models
+{
+    public static class AppSettings
+    {
+        public static string Url { get; set; }  = "https://github.com/CloudSurferCode/EverlastSummerTelegramBot.git";
+
+        public static string Name { get; set; } = "everlastsummer_bot";
+
+        public static string Key { get; set; }  = "518269622:AAGxaJPQuyexmrmHuF4Qo7Ixf1CU9dWI_Eo";
+
+    }
+}
